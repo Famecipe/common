b34ad54cb63fb123715dd70636c817b79806c0bc
rjwehrle <rjwehrle@gmail.com>
2022-11-17 21:01:28 -0500

    Initial common files

---
+++ IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Famecipe.Common
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> Get();
+        public Task<T> Get(string identifer);
+        public Task<T> Create(T obj);
+        public Task Update(string identifer, T obj);
+        public Task Delete(string identifer);
+    }
+}
